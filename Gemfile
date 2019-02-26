16a21136b4a276a25b1d9eda2c6bd4f7eb8c3c78
Franklin Yu <franklinyu@hotmail.com>
2019-02-25 23:27:15 -0500

    Serve Atom from Bugzilla XML

---
+++ Gemfile
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+gem 'builder'
+gem 'nokogiri', '~> 1.10'
+gem 'sinatra', '~> 2.0'
---
+++ app.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'open-uri'
+require 'rss'
+
+require 'nokogiri'
+require 'sinatra'
+
+# @param document [Nokogiri::XML::Document]
+# @param url [String]
+def atom_feed_from(document, url)
+  RSS::Maker.make('atom') do |maker|
+    maker.channel.updated = Time.parse(document.at_xpath('bugzilla/bug/delta_ts').content)
+    maker.channel.title = document.at_xpath('bugzilla/bug/short_desc').content
+    maker.channel.id = url
+    maker.channel.links.new_link do |l|
+      l.href = url
+      l.type = 'text/html'
+      l.rel = :alternative
+    end
+
+    document.xpath('bugzilla/bug/long_desc').each do |desc|
+      maker.items.new_item do |item|
+        id = desc.at_xpath('commentid').content.to_i
+
+        item.title = "comment ##{id}"
+        item.id = "#{url}##{id}"
+        item.author = desc.at_xpath('who')[:name]
+        item.updated = Time.parse(desc.at_xpath('bug_when').content)
+        item.content.content = desc.at_xpath('thetext').content
+        item.content.type = :text
+      end
+    end
+  end
+end
+
+get '/feed' do
+  content_type :atom
+
+  uri = URI.parse(params[:url])
+  query = URI.decode_www_form(uri.query)
+  query << [:ctype, :xml]
+  uri.query = URI.encode_www_form(query)
+  document = open(uri) { |f| Nokogiri::XML(f) }
+
+  atom_feed_from(document, params[:url]).to_s
+end
