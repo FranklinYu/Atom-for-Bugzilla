c03c5f83f1711ee868779805f28bfaf97bbac068
Franklin Yu <franklinyu@hotmail.com>
2019-05-24 00:51:18 -0400

    Refactor test cases

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -5,10 +5,18 @@ require 'rss'
 require_relative '../app'
 
 RSpec.describe 'The application' do
-  it 'works' do
-    get '/feed', url: 'https://bugzilla.mozilla.org/show_bug.cgi?id=256718'
-    expect(last_response).to be_ok
-    rss = RSS::Parser.parse(last_response.body)
-    expect(rss.title.content).to eq('Implement Atom feeds for bugs')
+  context 'with correct Bugzilla link' do
+    before(:example) do
+      get '/feed', url: 'https://bugzilla.mozilla.org/show_bug.cgi?id=256718'
+    end
+
+    it 'returns OK' do
+      expect(last_response).to be_ok
+    end
+
+    it 'shows coorrect title' do
+      rss = RSS::Parser.parse(last_response.body)
+      expect(rss.title.content).to eq('Implement Atom feeds for bugs')
+    end
   end
 end
